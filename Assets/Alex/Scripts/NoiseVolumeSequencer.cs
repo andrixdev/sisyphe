58ee81b9c0c72c764a61c1e3225ffab3055bde11
alex_bourgeois <abourgeois27380@gmail.com>
2020-09-28 21:36:56 +0200

    Tweaks

---
+++ Assets/Alex/Scripts/NoiseVolumeSequencer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoiseVolumeSequencer : MonoBehaviour
+{
+    public List<NoiseVolume> volumes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i=0; i<volumes.Count; i++)
+        {
+            volumes[i].enabled = false;
+            volumes[i].enabled = true;
+        }   
+    }
+}
